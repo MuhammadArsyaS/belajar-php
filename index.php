c9382c684345d266f8a4b36d8275bc8460e2ae4f
Muhammad Arsya S <benjaminclay44@gmail.com>
2022-10-24 21:08:56 +0700

    origin

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+  echo "Hello World! <br>";
+
+  $nama = "Muhammad Arsya S";
+  $umur = "21";
+
+  echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+  
+  $namaAdik = "Ahmad Mulahaqqi Soufandi";
+  $umurAdik = 14;
+
+  echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+  $selisihumur = $umur - $umurAdik;
+  echo "Selisih usia saya dan adik saya adalah $selisihumur tahun"
+  ?>
