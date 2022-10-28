80566e7dbddfd3d2e091c143fd3dabb6244b40a2
Muhammad Arsya S <benjaminclay44@gmail.com>
2022-10-28 22:29:17 +0700

    Praktik Materi Php Lanjuta

---
+++ php-lanjutan.php
@@ -0,0 +1,46 @@
+<?php
+$nama = "Arsya";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Input Nama dan Diulang</h1>
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+        <label>Nama</label>
+        <input type="text" name="nama">
+        <label>Jumlah</label>
+        <input type="text" name="no">
+        <input type="submit" name="submit" value="Submit">
+    </form>
+    <?php
+        if(!empty($_POST['submit'])) {
+
+            switch($_POST['nama']) {
+                case "Arsya":
+                    $pesan = $_POST['nama']." berasal dari Tuban";
+                break;
+                case "Aldo":
+                    $pesan = $_POST['nama']." berasal dari Malang";
+                break;
+                default:
+                    $pesan = $_POST['nama']." darimana ya?";
+            
+            }
+
+            for ($i=0;$i<$_POST['no'];$i++) {
+                echo $pesan."<br>";
+            }
+
+        } else {
+            echo "Silakan masukkan Nama dan Jumlah terlebih dahulu!";
+        }
+    ?>   
+</body>
+</html>
